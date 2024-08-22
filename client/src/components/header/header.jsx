e8ed549cf6ee889fd6fa802b70cf5057771eecc8
RenanFernandoRuiz <renanfernandor908@gmail.com>
2024-08-22 11:27:19 -0300

    helloWorld 1

---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import styles from "./header.module.css";
+import logo from "../../assets/logo.svg";
+
+function Header(){
+    return(
+        <header className={styles.header}>
+        <nav>
+            <div className={styles.navtop}>
+            <img src={logo} alt="Logo Hello World" />
+            
+            <a href="#">Hello World</a>
+            <a href="#">Cursos</a>
+            <a href="#">Sobre</a>
+            <a href="#">Desenvolvimento</a>
+            <a href="#">Contato</a>
+           
+            </div>
+
+           <div className={styles.idioma}>
+           <a href="#">Idioma</a>
+           </div>
+            
+        </nav>
+        </header>
+    )
+}
+
+export default Header;
