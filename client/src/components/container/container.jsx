3d496f353e23752f78c70f633b998b4e020f681a
RenanFernandoRuiz <renanfernandor908@gmail.com>
2024-08-29 09:32:04 -0300

    Desenvolvimento sidebar

--- client/src/components/container/container.jsx
+++ client/src/components/container/container.jsx
@@ -3,10 +3,20 @@ import quadro from "../../assets/quadro.jpg";
 
 const Container = () => {
   return (
-    <div className={styles.all}>
-      <img src={quadro} alt="tela" />
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
+    <body>
+      <div className={styles.sidebar}>
+        <a href="#">Hello World</a>
+        <a href="#">Cursos</a>
+        <a href="#">Sobre</a>
+        <a href="#">Desenvolvimento</a>
+        <a href="#">Contato</a>
+      </div>
+      
+      <div className={styles.all}>
+        <img src={quadro} alt="tela" />
+        <h2>O jeito grátis, divertido e eficaz de aprender programação!</h2>
+      </div>
+    </body>
   );
 };
 
