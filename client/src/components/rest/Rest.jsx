f483e9af39c0f369b1ea7c7dc9e5efc267f97f3a
RenanFernandoRuiz <renanfernandor908@gmail.com>
2024-08-30 15:28:50 -0300

    Ajustes no Rest

--- client/src/components/rest/Rest.jsx
+++ client/src/components/rest/Rest.jsx
@@ -1,19 +1,17 @@
-import styles from "./rest.module.css";
+import "./rest.css";
 import sim from "../../assets/sim.svg";
 
 const Rest = () => {
   return (
-    <div className={styles.resto}>
-      <div className={styles.restexto}>
+    <div className="resto bg-info d-flex row ">
+      <div className="restexto bg-warning col-5">
         <h2>Grátis e Divertido</h2>
         <h3>
-          Aprenda de forma criativa ideias
-           e dicas para programação de um jogo!
-          Nada melhor do que entender
-           como funciona o seu jogo!
+          Aprenda de forma criativa ideias e dicas para programação de um jogo!
+          Nada melhor do que entender como funciona o seu jogo!
         </h3>
       </div>
-      <div className={styles.sim}>
+      <div className="sim bg-black m-0 col-7">
         <img src={sim} alt="" />
       </div>
     </div>
