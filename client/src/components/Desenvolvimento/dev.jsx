8c564f0f717734b9b946177190314da514b7a07b
PietroDub <pietroti453@gmail.com>
2024-09-11 16:52:26 -0300

    criado várias páginas

---
+++ client/src/components/Desenvolvimento/dev.jsx
@@ -0,0 +1,49 @@
+import "./dev.css"
+import {h2} from "../../assets/h2.svg"
+
+const Dev = () => {
+    return (
+        <div>
+        <div className="Headev col-12">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+            <h1>
+                Desenvolvimento
+            </h1>
+        </div>
+        <div className="fundin col-12">
+<div className="container col-10">
+<img src={h2}></img>
+<div className="direito col">
+<h1>DESENVOLVIDO PERSONALIZAÇÃO DE AVATAR:</h1>
+<h2>Os avatares peronalizados foram criados ,assim cada usuário poderá ter sua própria experiencia.(04/02/2026)</h2>
+<button>SAIBA MAIS</button>
+</div>
+</div>
+        </div>
+        <div className="fundin col-12">
+<div className="container col-10">
+<img src={h2}></img>
+<div className="direito col">
+<h1>DESENVOLVIDO PERSONALIZAÇÃO DE AVATAR:</h1>
+<h2>Os avatares peronalizados foram criados ,assim cada usuário poderá ter sua própria experiencia.(04/02/2026)</h2>
+<button>SAIBA MAIS</button>
+</div>
+</div>
+        </div>
+        <div className="fundin col-12">
+<div className="container col-10">
+<img src={h2}></img>
+<div className="direito col">
+<h1>DESENVOLVIDO PERSONALIZAÇÃO DE AVATAR:</h1>
+<h2>Os avatares peronalizados foram criados ,assim cada usuário poderá ter sua própria experiencia.(04/02/2026)</h2>
+<button>SAIBA MAIS</button>
+</div>
+</div>
+        </div>
+        </div>
+        
+    );
+    
+}
+
+export default  Dev
