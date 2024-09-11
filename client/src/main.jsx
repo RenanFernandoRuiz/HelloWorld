d9db269cc57a96e5814c39725caa9e2aaea1f9c3
PietroDub <pietroti453@gmail.com>
2024-09-10 21:56:23 -0300

    iniciado o projeto em casa

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Contatos from "./pages/Contatos.jsx";
 
 import Home from "./pages/Home.jsx";
-import Contato from "./pages/Contato.jsx";
 
 const router = createBrowserRouter([
   {
@@ -15,10 +15,10 @@ const router = createBrowserRouter([
     // errorElement: <PaginaErro/>,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/contato", element: <Contato /> },
-      { path: "/sobre", element: <Contato /> },
-      { path: "/desenvolvimento", element: <Contato /> },
-      { path: "/cursos", element: <Contato /> },
+      { path: "/contato", element: <Contatos/> },
+      { path: "/sobre", element: <Contatos /> },
+      { path: "/desenvolvimento", element: <Contatos /> },
+      { path: "/cursos", element: <Contatos /> },
       // { path: "contact", element: <Contact /> },
       // { path: "contact/:id", element: <ContactDetails /> },
     ],
