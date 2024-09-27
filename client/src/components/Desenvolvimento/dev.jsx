400f9f64cba343ffc49f3599dd23a5ce7a5f6654
RenanFernandoRuiz <renanfernandor908@gmail.com>
2024-09-27 11:47:51 -0300

    Criação do Desenvolvimeto mobile

--- client/src/components/Desenvolvimento/dev.jsx
+++ client/src/components/Desenvolvimento/dev.jsx
@@ -10,7 +10,7 @@ const Dev = () => {
         <ion-icon name="arrow-back-outline"></ion-icon>
         <h1>Desenvolvimento</h1>
       </div>
-      <div className="fundingera">
+      <div className="fundingera ">
         <div className="fundin col-12 d-flex">
           <div className="container col-10">
             <img src={h2}></img>
@@ -43,7 +43,7 @@ const Dev = () => {
             <div className="direito col">
               <h1>DESENVOLVIDO PERSONALIZAÇÃO DE AVATAR:</h1>
               <h2>
-                Os avatares peronalizados foram criados ,assim cada usuário
+                Os avatares personalizados foram criados ,assim cada usuário
                 poderá ter sua própria experiencia.(04/02/2026)
               </h2>
               <button>SAIBA MAIS</button>
--- client/src/components/Home/rest/Rest.jsx
+++ client/src/components/Home/rest/Rest.jsx
@@ -2,6 +2,8 @@ import "./rest.css";
 import sim from "../../../assets/sim.svg";
 import leptop from "../../../assets/leptop.svg";
 import celular from "../../../assets/celular.svg";
+import h2 from "../../../assets/h2.svg";
+
 
 const Rest = () => {
   return (
@@ -26,6 +28,45 @@ const Rest = () => {
         <div className="restexto col-12 col-md-5 ">
           <h3>Baseado e inspirado em jogos de todo o mundo!</h3>
         </div>
+
+
+
+
+        <div id="desenvolvimento" className="Header col-12 justify-content-center align-items-center d-lg-none   ">
+          <h1>ULTIMA NOTÍCIA:</h1>
+        </div>
+
+        <div className="fundo d-lg-none  ">
+          <div className="cordefundo col-12 d-flex d-lg-none">
+            <div className="containers col-10 d-lg-none">
+            <img src={h2}></img>
+            <div className="alinhar col d-lg-none ">
+             
+              <h2 >
+                Os avatares peronalizados foram criados ,assim cada usuário
+                poderá ter sua própria experiencia.(04/02/2026)
+              </h2>
+            </div>
+
+            
+         
+            </div>
+
+
+          </div>
+
+          <div className="noticias">
+
+            <h2>PARA MAIS NOTÍCIAS , ACESSE EM UM DISPOSITIVO MAIOR.</h2>
+          </div>
+
+        </div>
+         
+           
+        
+      
+
+
       </div>
       <div id="instalar" className="tre col-12 row ">
         <div className="simimg col-12 col-md-6">
@@ -35,8 +76,8 @@ const Rest = () => {
           <h2>Aprenda de todos os lugares!</h2>
           <h3>
             Temos aplicativo disponivel para dispositivos moveis! Permitindo que
-            vc realize as atividades e quizes de qualquer lugar que vocÊ
-            eestiver!
+            vc realize as atividades e quizes de qualquer lugar que você
+            estiver!
           </h3>
         </div>
         <button className="col-1">Instale nosso aplicativo!</button>
